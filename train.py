a23971708dd6f959a56c9b4735993d34057681ff
xdongming <97573465+xdongming@users.noreply.github.com>
2022-05-21 17:45:05 +0800

    Update train.py

--- train.py
+++ train.py
@@ -5,29 +5,32 @@ import tensorflow as tf
 import os
 import cv2
 import time
+import copy
 import collections
 import matplotlib.pyplot as plt
 
 from Model import Model
 from DQN import DQN
+from tensorflow.keras.models import Model as mod
 from Agent import Agent
 from ReplayMemory import ReplayMemory
-
+from Tool.GetMetrics import get_pass_count
 
 import Tool.Helper
 import Tool.Actions
 from Tool.Helper import mean, is_end
-from Tool.Actions import take_action, restart,take_direction, TackAction
+from Tool.Actions import take_action, restart, take_direction, TackAction
 from Tool.WindowsAPI import grab_screen
 from Tool.GetHP import Hp_getter
 from Tool.UserInput import User
 from Tool.FrameBuffer import FrameBuffer
 
-window_size = (0,0,1920,1017)
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # 注释这行将使用GPU运算
+window_size = (0, 0, 1920, 1017)
 station_size = (230, 230, 1670, 930)
 
-HP_WIDTH = 768
-HP_HEIGHT = 407
+# HP_WIDTH = 768
+# HP_HEIGHT = 407
 WIDTH = 400
 HEIGHT = 200
 ACTION_DIM = 7
@@ -36,36 +39,35 @@ INPUT_SHAPE = (FRAMEBUFFERSIZE, HEIGHT, WIDTH, 3)
 
 MEMORY_SIZE = 200  # replay memory的大小，越大越占用内存
 MEMORY_WARMUP_SIZE = 24  # replay_memory 里需要预存一些经验数据，再从里面sample一个batch的经验让agent去learn
-BATCH_SIZE = 10  # 每次给agent learn的数据数量，从replay memory随机里sample一批数据出来
+BATCH_SIZE = 4  # 每次给agent learn的数据数量，从replay memory随机里sample一批数据出来
 LEARNING_RATE = 0.00001  # 学习率
-GAMMA = 0
+GAMMA = 0.8
 
 action_name = ["Attack", "Attack_Up",
-           "Short_Jump", "Mid_Jump", "Skill_Up", 
-           "Skill_Down", "Rush", "Cure"]
+               "Short_Jump", "Mid_Jump", "Skill_Up",
+               "Skill_Down", "Rush", "Cure"]
 
 move_name = ["Move_Left", "Move_Right", "Turn_Left", "Turn_Right"]
 
 DELAY_REWARD = 1
 
 
-
-
-def run_episode(hp, algorithm,agent,act_rmp_correct, move_rmp_correct,PASS_COUNT,paused):
-    restart()
+def run_episode(hp, algorithm, agent, act_rmp_correct, move_rmp_correct, PASS_COUNT, paused, model):
     # learn while load game
     for i in range(8):
         if (len(move_rmp_correct) > MEMORY_WARMUP_SIZE):
             # print("move learning")
-            batch_station,batch_actions,batch_reward,batch_next_station,batch_done = move_rmp_correct.sample(BATCH_SIZE)
-            algorithm.move_learn(batch_station,batch_actions,batch_reward,batch_next_station,batch_done)   
+            batch_station, batch_actions, batch_reward, batch_next_station, batch_done = move_rmp_correct.sample(
+                BATCH_SIZE)
+            algorithm.move_learn(batch_station, batch_actions, batch_reward, batch_next_station, batch_done)
 
         if (len(act_rmp_correct) > MEMORY_WARMUP_SIZE):
             # print("action learning")
-            batch_station,batch_actions,batch_reward,batch_next_station,batch_done = act_rmp_correct.sample(BATCH_SIZE)
-            algorithm.act_learn(batch_station,batch_actions,batch_reward,batch_next_station,batch_done)
+            batch_station, batch_actions, batch_reward, batch_next_station, batch_done = act_rmp_correct.sample(
+                BATCH_SIZE)
+            algorithm.act_learn(batch_station, batch_actions, batch_reward, batch_next_station, batch_done)
+    restart()
 
-    
     step = 0
     done = 0
     total_reward = 0
@@ -74,21 +76,21 @@ def run_episode(hp, algorithm,agent,act_rmp_correct, move_rmp_correct,PASS_COUNT
     # Delay Reward
     DelayMoveReward = collections.deque(maxlen=DELAY_REWARD)
     DelayActReward = collections.deque(maxlen=DELAY_REWARD)
-    DelayStation = collections.deque(maxlen=DELAY_REWARD + 1) # 1 more for next_station
+    DelayStation = collections.deque(maxlen=DELAY_REWARD + 1)  # 1 more for next_station
     DelayActions = collections.deque(maxlen=DELAY_REWARD)
     DelayDirection = collections.deque(maxlen=DELAY_REWARD)
-    
+
     while True:
         boss_hp_value = hp.get_boss_hp()
         self_hp = hp.get_self_hp()
-        if boss_hp_value > 800 and  boss_hp_value <= 900 and self_hp >= 1 and self_hp <= 9:
+        if boss_hp_value > 800 and boss_hp_value <= 900 and self_hp >= 1 and self_hp <= 9:
             break
-        
 
     thread1 = FrameBuffer(1, "FrameBuffer", WIDTH, HEIGHT, maxlen=FRAMEBUFFERSIZE)
     thread1.start()
 
     last_hornet_y = 0
+    flag = 0
     while True:
         step += 1
         # last_time = time.time()
@@ -97,10 +99,10 @@ def run_episode(hp, algorithm,agent,act_rmp_correct, move_rmp_correct,PASS_COUNT
         #     break
 
         # in case of do not collect enough frames
-        
-        while(len(thread1.buffer) < FRAMEBUFFERSIZE):
+
+        while (len(thread1.buffer) < FRAMEBUFFERSIZE):
             time.sleep(0.1)
-        
+
         stations = thread1.get_buffer()
         boss_hp_value = hp.get_boss_hp()
         self_hp = hp.get_self_hp()
@@ -115,14 +117,13 @@ def run_episode(hp, algorithm,agent,act_rmp_correct, move_rmp_correct,PASS_COUNT
 
         move, action = agent.sample(stations, soul, hornet_x, hornet_y, player_x, hornet_skill1)
 
-        
         # action = 0
         take_direction(move)
         take_action(action)
-        
+
         # print(time.time() - start_time, " action: ", action_name[action])
         # start_time = time.time()
-        
+
         next_station = thread1.get_buffer()
         next_boss_hp_value = hp.get_boss_hp()
         next_self_hp = hp.get_self_hp()
@@ -130,12 +131,14 @@ def run_episode(hp, algorithm,agent,act_rmp_correct, move_rmp_correct,PASS_COUNT
         next_hornet_x, next_hornet_y = hp.get_hornet_location()
 
         # get reward
-        move_reward = Tool.Helper.move_judge(self_hp, next_self_hp, player_x, next_player_x, hornet_x, next_hornet_x, move, hornet_skill1)
+        move_reward = Tool.Helper.move_judge(self_hp, next_self_hp, player_x, next_player_x, hornet_x, next_hornet_x,
+                                             move, hornet_skill1)
         # print(move_reward)
-        act_reward, done = Tool.Helper.action_judge(boss_hp_value, next_boss_hp_value,self_hp, next_self_hp, next_player_x, next_hornet_x,next_hornet_x, action, hornet_skill1)
-            # print(reward)
+        act_reward, done = Tool.Helper.action_judge(boss_hp_value, next_boss_hp_value, self_hp, next_self_hp,
+                                                    next_player_x, next_hornet_x, next_hornet_x, action, hornet_skill1)
+        # print(reward)
         # print( action_name[action], ", ", move_name[d], ", ", reward)
-        
+
         DelayMoveReward.append(move_reward)
         DelayActReward.append(act_reward)
         DelayStation.append(stations)
@@ -144,20 +147,19 @@ def run_episode(hp, algorithm,agent,act_rmp_correct, move_rmp_correct,PASS_COUNT
 
         if len(DelayStation) >= DELAY_REWARD + 1:
             if DelayMoveReward[0] != 0:
-                move_rmp_correct.append((DelayStation[0],DelayDirection[0],DelayMoveReward[0],DelayStation[1],done))
+                move_rmp_correct.append((DelayStation[0], DelayDirection[0], DelayMoveReward[0], DelayStation[1], done))
             # if DelayMoveReward[0] <= 0:
             #     move_rmp_wrong.append((DelayStation[0],DelayDirection[0],DelayMoveReward[0],DelayStation[1],done))
 
         if len(DelayStation) >= DELAY_REWARD + 1:
             if mean(DelayActReward) != 0:
-                act_rmp_correct.append((DelayStation[0],DelayActions[0],mean(DelayActReward),DelayStation[1],done))
+                act_rmp_correct.append((DelayStation[0], DelayActions[0], mean(DelayActReward), DelayStation[1], done))
             # if mean(DelayActReward) <= 0:
             #     act_rmp_wrong.append((DelayStation[0],DelayActions[0],mean(DelayActReward),DelayStation[1],done))
 
         station = next_station
         self_hp = next_self_hp
         boss_hp_value = next_boss_hp_value
-            
 
         # if (len(act_rmp) > MEMORY_WARMUP_SIZE and int(step/ACTION_SEQ) % LEARN_FREQ == 0):
         #     print("action learning")
@@ -172,23 +174,27 @@ def run_episode(hp, algorithm,agent,act_rmp_correct, move_rmp_correct,PASS_COUNT
             break
         elif done == 2:
             PASS_COUNT += 1
+            flag = 1
             Tool.Actions.Nothing()
             time.sleep(3)
             break
-        
 
     thread1.stop()
+
+    get_pass_count(flag)
 
     for i in range(8):
         if (len(move_rmp_correct) > MEMORY_WARMUP_SIZE):
             # print("move learning")
-            batch_station,batch_actions,batch_reward,batch_next_station,batch_done = move_rmp_correct.sample(BATCH_SIZE)
-            algorithm.move_learn(batch_station,batch_actions,batch_reward,batch_next_station,batch_done)   
+            batch_station, batch_actions, batch_reward, batch_next_station, batch_done = move_rmp_correct.sample(
+                BATCH_SIZE)
+            algorithm.move_learn(batch_station, batch_actions, batch_reward, batch_next_station, batch_done)
 
         if (len(act_rmp_correct) > MEMORY_WARMUP_SIZE):
             # print("action learning")
-            batch_station,batch_actions,batch_reward,batch_next_station,batch_done = act_rmp_correct.sample(BATCH_SIZE)
-            algorithm.act_learn(batch_station,batch_actions,batch_reward,batch_next_station,batch_done)
+            batch_station, batch_actions, batch_reward, batch_next_station, batch_done = act_rmp_correct.sample(
+                BATCH_SIZE)
+            algorithm.act_learn(batch_station, batch_actions, batch_reward, batch_next_station, batch_done)
     # if (len(move_rmp_wrong) > MEMORY_WARMUP_SIZE):
     #     # print("move learning")
     #     batch_station,batch_actions,batch_reward,batch_next_station,batch_done = move_rmp_wrong.sample(1)
@@ -206,26 +212,24 @@ if __name__ == '__main__':
 
     # In case of out of memory
     config = tf.compat.v1.ConfigProto(allow_soft_placement=True)
-    config.gpu_options.allow_growth = True      #程序按需申请内存
-    sess = tf.compat.v1.Session(config = config)
+    config.gpu_options.allow_growth = True  # 程序按需申请内存
+    sess = tf.compat.v1.Session(config=config)
 
-    
     total_remind_hp = 0
 
-    act_rmp_correct = ReplayMemory(MEMORY_SIZE, file_name='./act_memory')         # experience pool
-    move_rmp_correct = ReplayMemory(MEMORY_SIZE,file_name='./move_memory')         # experience pool
-    
+    act_rmp_correct = ReplayMemory(MEMORY_SIZE, file_name='./act_memory')  # experience pool
+    move_rmp_correct = ReplayMemory(MEMORY_SIZE, file_name='./move_memory')  # experience pool
+
     # new model, if exit save file, load it
-    model = Model(INPUT_SHAPE, ACTION_DIM)  
+    model = Model(INPUT_SHAPE, ACTION_DIM)
 
     # Hp counter
     hp = Hp_getter()
 
-
     model.load_model()
     algorithm = DQN(model, gamma=GAMMA, learnging_rate=LEARNING_RATE)
-    agent = Agent(ACTION_DIM,algorithm,e_greed=0.12,e_greed_decrement=1e-6)
-    
+    agent = Agent(ACTION_DIM, algorithm, e_greed=0.12, e_greed_decrement=1e-6)
+
     # get user input, no need anymore
     # user = User()
 
@@ -236,20 +240,19 @@ if __name__ == '__main__':
     max_episode = 30000
     # 开始训练
     episode = 0
-    PASS_COUNT = 0                                       # pass count
-    while episode < max_episode:    # 训练max_episode个回合，test部分不计算入episode数量
+    PASS_COUNT = 0  # pass count
+    while episode < max_episode:  # 训练max_episode个回合，test部分不计算入episode数量
         # 训练
-        episode += 1     
-        # if episode % 20 == 1:
-        #     algorithm.replace_target()
-
-        total_reward, total_step, PASS_COUNT, remind_hp = run_episode(hp, algorithm,agent,act_rmp_correct, move_rmp_correct, PASS_COUNT, paused)
-        if episode % 10 == 1:
+        episode += 1
+        total_reward, total_step, PASS_COUNT, remind_hp = run_episode(hp, algorithm, agent, act_rmp_correct,
+                                                                      move_rmp_correct, PASS_COUNT, paused, model)
+        if episode % 5 == 0:
             model.save_mode()
         if episode % 5 == 0:
+            model.load_model()
+        if episode % 3 == 0:
             move_rmp_correct.save(move_rmp_correct.file_name)
-        if episode % 5 == 0:
+        if episode % 3 == 0:
             act_rmp_correct.save(act_rmp_correct.file_name)
         total_remind_hp += remind_hp
-        print("Episode: ", episode, ", pass_count: " , PASS_COUNT, ", hp:", total_remind_hp / episode)
-
+        print("Episode: ", episode, ", pass_count: ", PASS_COUNT, ", hp:", total_remind_hp / episode)
